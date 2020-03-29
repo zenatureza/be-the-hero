8278334538dce71a37e5da91d6e0ae0b9f9f5940
zenatureza <arthur.pinheiro357@gmail.com>
2020-03-29 16:59:24 -0300

    testando

--- mobile/src/routes.js
+++ mobile/src/routes.js
@@ -3,6 +3,8 @@ import React from 'react';
 import { NavigationContainer } from '@react-navigation/native';
 import { createStackNavigator } from '@react-navigation/stack';
 
+//teste
+
 const AppStack = createStackNavigator();
 
 import Incidents from './pages/Incidents';
